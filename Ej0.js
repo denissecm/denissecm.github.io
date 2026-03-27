ab0aa5aaf97d099a09dfe469176bcf3930329852
denissecm <A01639459@tec.mx>
2026-03-26 18:09:31 -0600

    Create Ej0.js

---
+++ Ej0.js
@@ -0,0 +1,19 @@
+import http from 'http';
+
+const servidor = http.createServer(async (req, res) => {
+    if (req.url === '/usuarios') {
+            const respuesta = await fetch('https://jsonplaceholder.typicode.com/users');
+            const datos = await respuesta.json();
+
+            const resultado = datos.map(usuario => ({
+                nombre: usuario.name,
+                correo: usuario.email
+            }));
+      
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(resultado));
+    } else {
+        res.writeHead(404);
+        res.end('Ruta no encontrada');
+    }
+});
